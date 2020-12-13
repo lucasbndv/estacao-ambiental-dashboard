654e4c3b9bad508e3a861be9e54fe65abfe7fc8f
lucasbndv <lucasbndv@gmail.com>
2020-12-12 23:55:19 -0300

    Table updating with filters

--- server/routes/db_routes.js
+++ server/routes/db_routes.js
@@ -11,12 +11,29 @@ const db = new sqlite3.Database(
   }
 );
 
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   let filters = req.body;
   console.log(filters);
+
+  if (
+    filters.date_from === "" ||
+    filters.date_to === "" ||
+    filters.sample === ""
+  ) {
+    if (filters.date_from === "") {
+      filters.date_from = "2020-10-08";
+    }
+    if (filters.date_to === "") {
+      filters.date_to = "2022-10-08";
+    }
+    if (filters.sample === "") {
+      filters.sample = "20";
+    }
+  }
+
   db.all(
-    "SELECT * FROM estacao1 WHERE Data BETWEEN ? AND ? ORDER BY rowid LIMIT ?",
-    [filters.date_from, filters.date_to, filters.limit],
+    "SELECT * FROM estacao1 WHERE Data BETWEEN ? AND ? ORDER BY rowid ? LIMIT ?",
+    [filters.date_from, filters.date_to, filters.desc, filters.sample],
     (err, data) => {
       if (err) {
         console.error("Error on GET 'api/datas'");
@@ -29,6 +46,39 @@ router.get("/", (req, res) => {
       console.log("data was sent");
     }
   );
+});
+
+router.post("/all_param", (req, res) => {
+  let filters = req.body;
+  console.log(filters);
+  db.all(
+    "SELECT * FROM estacao1 WHERE Data BETWEEN ? AND ? ORDER BY rowid ? LIMIT ?",
+    [filters.date_from, filters.date_to, filters.desc, filters.sample],
+    (err, data) => {
+      if (err) {
+        console.error("Error on GET 'api/datas'");
+        res.status(400).json({ error: err.message });
+        process.exit(1);
+      }
+
+      last_data = data;
+      res.send(data);
+      console.log("data was sent");
+    }
+  );
+});
+router.post("/all", (req, res) => {
+  db.all("SELECT * FROM estacao1", [], (err, data) => {
+    if (err) {
+      console.error("Error on GET 'api/datas'");
+      res.status(400).json({ error: err.message });
+      process.exit(1);
+    }
+
+    last_data = data;
+    res.send(data);
+    console.log("data was sent");
+  });
 });
 
 module.exports = router;
--- webpage/scripts/main.js
+++ webpage/scripts/main.js
@@ -2,66 +2,80 @@ let socket = io();
 let dataTable = new DataTable();
 let dataSet = {};
 
-async function getData(url) {
-    let res = await fetch(url, {
-    method: "GET", 
+// auxiliar functions
+
+async function getData(url, config) {
+  let res = await fetch(url, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
-    }});
-    
+    },
+    body: JSON.stringify(config),
+  });
+
   let text = await res.text();
   let data = JSON.parse(text);
-  
+
   return data;
 }
 
 async function generateTable(table, data) {
   let div = document.querySelector("div.table_container");
+
   div.appendChild(table.table);
-  
   table.table.id = "customers";
-  
-  data.forEach(d => table.insertRow(d));
-  
+
+  data.forEach((d) => table.insertRow(d));
+
   table.headers = Object.keys(data[0]);
-  table.headers.forEach(h => {
+  table.headers.forEach((h) => {
     table.insertHeader(h);
   });
-  
-  //console.log(dataSet["Alt"]);
 }
 
-function loadCSS(url,doc) {
-  doc.setAttribute('type', "text/css" );
-  doc.setAttribute('rel', "stylesheet" );
-  doc.setAttribute('href', url );
+async function updateTable(table, data) {
+  let rows = table.table.rows.length;
+  let j = 0;
+  for (let i = 0; i < rows; i++) {
+    table.deleteRowPop();
+    j++;
+  }
+  console.log(rows);
+  console.log(j);
+
+  data.forEach((d) => table.insertRow(d));
+}
+
+function loadCSS(url, doc) {
+  doc.setAttribute("type", "text/css");
+  doc.setAttribute("rel", "stylesheet");
+  doc.setAttribute("href", url);
   document.getElementsByTagName("head").item(0).appendChild(doc);
 }
 
 function initSocketIO(socket) {
-  socket.on("update_table",(data) => {
-    
+  socket.on("update_table", (data) => {
     data = JSON.parse(data)[0];
     //console.log(data);
 
     dataTable.deleteRowPop();
     dataTable.insertRow(data);
-    
-    Object.keys(dataSet).forEach(k => {
+
+    Object.keys(dataSet).forEach((k) => {
       dataSet[k].pop();
       dataSet[k].unshift(data[k]);
     });
-    
+
     //updateChart(window.chart, dataBase);
     window.chart.update();
-  })
+  });
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.floor(Math.random() * 16)];
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
@@ -69,38 +83,72 @@ function getRandomColor() {
 function setupChart(chart, dataSet) {
   let keys = Object.keys(dataSet);
   let label = keys[0];
-  
-  //console.log(Object.keys(dataSet));
+
   chart.data.labels = dataSet[label];
-  
+
   chart.data.datasets = [];
-  keys.forEach(k => {
+  keys.forEach((k) => {
     d = {
       label: k,
-      data: dataSet[k]
+      data: dataSet[k],
     };
-    
+
     chart.data.datasets.push(d);
   });
 
   chart.update();
 }
 
-async function main() {
-  let data = await getData("/api/measure/estacao1");
-  
-  generateTable(dataTable, data);
-  //dataTable.table.style.display = "none";
-  
-  Object.keys(data[0]).forEach(k => {
-    dataSet[k] = data.map(d => d[k]);
+function setupEvents() {
+  let search = document.getElementById("search");
+
+  search.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    let date_from = document.getElementById("date_from").value;
+    let date_to = document.getElementById("date_to").value;
+    let sample = document.getElementById("sample").value;
+    let desc = document.getElementById("desc").checked;
+    let all = false;
+
+    if (desc) {
+      desc = "DESC";
+    } else {
+      desc = "ASC";
+    }
+
+    if (sample === "") {
+      all = true;
+    }
+    let config = { date_from, date_to, sample, desc, all };
+
+    console.log(desc);
+
+    let data = await getData("/db", config);
+
+    updateTable(dataTable, data);
+
+    Object.keys(data[0]).forEach((k) => {
+      dataSet[k] = data.map((d) => d[k]);
+    });
+
+    console.log(dataSet);
+
+    //setupChart(window.chart, dataSet);
   });
-    
-  //console.log(dataSet);
-  //console.log(Object.keys(dataSet));
-  //console.log(dataSet["Data"]);
-  
-  setupChart(window.chart, dataSet);
+}
+
+// main
+async function main() {
+  let data = await getData("/db/all", { limit: 15 });
+
+  Object.keys(data[0]).forEach((k) => {
+    dataSet[k] = data.map((d) => d[k]);
+  });
+  generateTable(dataTable, data);
+
+  //setupChart(window.chart, dataSet);
+  setupEvents();
   initSocketIO(socket);
 }
 
